d05257ec76cf0b8b5faff9fe4b6866b8b8a4d3b1
Rustavelli01 <Hec8832@hmail.com>
2024-01-09 22:28:41 +0600

    added Task1

---
+++ Seminar01/TASK01/Program.cs
@@ -0,0 +1,19 @@
+﻿// int number = 9;
+// int result = number * number;
+
+
+
+Console.Write("Введите первое число: ");
+int firstNum  = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNum  = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum == secondNum * secondNum)
+{
+    Console.WriteLine("Да, " + firstNum + " является кв. от числа " + secondNum );
+}
+else 
+{
+    Console.WriteLine("Нет, " + firstNum + " не является кв. от числа " + secondNum );
+}
