1e4042c04cf393bd8c537d839bb55c455ec418c2
Rustavelli01 <Hec8832@hmail.com>
2024-01-10 00:28:37 +0600

    added Seminar01 Task02

---
+++ Seminar01/TASK02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N<0)
+{
+    Console.WriteLine("Введено отрецательное число N! ");
+    N = N*-1;
+}
+int negativN = N * -1;
+while (negativN <= N)
+{
+    Console.WriteLine(negativN + " ");
+    negativN++;
+}
