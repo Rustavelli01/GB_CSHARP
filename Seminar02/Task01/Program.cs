7e46ea5c8e1ddad1e30fcbd4aa8d67b70a7b8fdb
Rustavelli01 <Hec8832@hmail.com>
2024-01-10 20:28:07 +0600

    Added Seminar02_Task03

---
+++ Seminar02/Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    int result = firstDigit * 10 + thirdDigit;
+    //Интерполяция: $ "Значение переменной{thirdDigit}={} "
+    Console.WriteLine($"Результат от числа {number} = {result}");
+}
+else
+{
+    Console.WriteLine("Число не 3-х значное");
+}
---
+++ Seminar02/Task03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Получите 3-ю цивру введенного числа(с начала)
+// 1234567 =>3
+// 7891=>9
+// 32=> нет
+
+Console.Write("введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int copyNumber = number;
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет :)");
+}
+ else
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    Console.WriteLine($"{copyNumber}={number % 10}");
+}
