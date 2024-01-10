127c8ffabdc6e58f358550969e9188bb01fba478
Rustavelli01 <Hec8832@hmail.com>
2024-01-10 19:50:43 +0600

    Added Seminar02_Task02

---
+++ Seminar02/Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿ Console.Write("введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int secondDigit = number / 10 % 10;
+    int thirdDigit = number % 10;
+   // Math.Pow(5,2) => 5 в 2 степени => 25
+   int result = (int)Math.Pow(secondDigit, thirdDigit);
+   Console.WriteLine($"Число{number}: {secondDigit}^{thirdDigit} = {result}");
+}
+else
+{
+    Console.WriteLine("Число не 3-х значное");
+}   
