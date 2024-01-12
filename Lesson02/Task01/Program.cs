86d7149b7d36768438db3ca5d3549203e85b7bba
Rustavelli01 <Hec8832@hmail.com>
2024-01-12 18:13:23 +0600

    Lesson02_Task01

---
+++ Lesson02/Task01/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = new int[5];
+array[0] = 5;
+array[1] = 7;
+array[2] = 5;
+array[3] = 4;
+array[4] = 3;
+
+Console.WriteLine(array[0]);
+Console.WriteLine(array[1]);
+Console.WriteLine(array[2]);
+Console.WriteLine(array[3]);
+Console.WriteLine(array[4]);
+
+//Console.WriteLine(array[5]);
+
+int[] array2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] array3 = { 1, 2, 3, 4, 5 }
+
