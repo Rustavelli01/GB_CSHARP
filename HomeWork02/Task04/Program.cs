8060eb12e530c83604ca15888580f92e10e18c5a
Rustavelli01 <Hec8832@hmail.com>
2024-01-12 15:53:02 +0600

    HomeWork02_Task04.MoreQuastion

---
+++ HomeWork02/Task04/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+
+Console.Clear();
+
+
+Console.Write("введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number < 10)
+{
+    Console.WriteLine(number);
+}
+else
+{
+    while (number > 0)
+    {
+        int copyNumber = number % 10;
+        number /= 10;
+        if (number > 0)
+        {
+            Console.Write(copyNumber + ", ");
+        }
+        else
+        {
+            Console.WriteLine(copyNumber);
+        }
+    }
+}
+
+// Не получается развернуть в обратном направлении
