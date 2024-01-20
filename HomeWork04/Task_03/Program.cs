4e62968bb29b90d30bcb3d4f19298a87fcc2d196
Rustavelli01 <Hec8832@hmail.com>
2024-01-20 17:35:30 +0600

    ихера не получилась 3-я задача

---
+++ HomeWork04/Task_03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+int[] numbers = {1, 3, 5, 6, 7, 8}; 
+int temp;
+
+Console.Write("Заданный массив: ");
+foreach (int number in numbers)
+{
+Console.Write(number + " ");
+}
+
+for (int i = 0; i < numbers.Length / 2; i++)
+{
+
+temp = numbers[i];
+numbers[i] = numbers[numbers.Length - 1 - i];
+numbers[numbers.Length - 1 - i] = temp;
+}
+Console.WriteLine();
+Console.Write("Развернутый массив: ");
+foreach (int number in numbers)
+{
+Console.Write(number + " ");
+}
